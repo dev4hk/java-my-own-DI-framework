e4babf378fe41761d3decb4c37471197646753fc
Hoon <hoon0661@gmail.com>
2024-03-31 09:37:01 -0400

    added dummy bean factory

--- src/main/java/org/example/di/BeanFactory.java
+++ src/main/java/org/example/di/BeanFactory.java
@@ -1,4 +1,20 @@
 package org.example.di;
 
+import org.example.controller.UserController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class BeanFactory {
+    private final Set<Class<?>> preInstantiatedClasses;
+    private Map<Class<?>, Object> beans = new HashMap<>();
+
+    public BeanFactory(Set<Class<?>> preInstantiatedClasses) {
+        this.preInstantiatedClasses = preInstantiatedClasses;
+    }
+
+    public <T> T getBean(Class<?> requiredType) {
+        return (T) beans.get(requiredType);
+    }
 }
